5e30333c82ef7cfe961d7eb605902a239a67a6f7
BatesonLights <59632966+SteelJ0ker@users.noreply.github.com>
2025-12-01 17:30:49 -0500

    Create rf-config.js

---
+++ rf-config.js
@@ -0,0 +1,9 @@
+// rf-config.js
+// Change this value and push to GitHub when you want to override behavior.
+// "auto"     = follow season + schedule (normal)
+// "forceOn"  = always on-air
+// "forceOff" = always off-air
+
+window.RF_OVERRIDE_MODE = "auto";
+// window.RF_OVERRIDE_MODE = "forceOn";
+// window.RF_OVERRIDE_MODE = "forceOff";
