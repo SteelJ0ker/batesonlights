84215cbebbe1d78e6050c000e237e085c741380f
BatesonLights <59632966+SteelJ0ker@users.noreply.github.com>
2025-12-02 14:57:34 -0500

    Delete rf-config.js

--- rf-config.js
+++
@@ -1,9 +0,0 @@
-// rf-config.js
-// Change this value and push to GitHub when you want to override behavior.
-// "auto"     = follow season + schedule (normal)
-// "forceOn"  = always on-air
-// "forceOff" = always off-air
-
-//window.RF_OVERRIDE_MODE = "auto";
-window.RF_OVERRIDE_MODE = "forceOn";
-// window.RF_OVERRIDE_MODE = "forceOff";
